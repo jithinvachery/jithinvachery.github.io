f2c258211b42eec33aae02cefdcaf53dab85e79e
Jithin Vachery <jithinvachery@gmail.com>
2023-07-25 13:14:48 +0800

    established connection with websocket server and sent data

--- CG4002/helper.js
+++ CG4002/helper.js
@@ -16,6 +16,51 @@ function terminate() {
   }
 
 /*
+    function to establish the connection to the websocket
+*/
+function startConnection() {
+    //const server_address = "ws://172.25.76.133:8001/"
+    const server_address = "ws://cg4002-i.comp.nus.edu.sg:8001/"
+    if ("WebSocket" in window) {
+       // Let us open a web socket
+       var ws = new WebSocket(server_address);
+
+       ws.onopen = function() {
+            // Web Socket is connected, send data using send()
+            var obj = new Object();
+            obj.group_name  = sessionStorage.group_name;
+            obj.num_player  = sessionStorage.num_player;
+            obj.password    = sessionStorage.password;
+
+            //check to see if we are starting a new connection
+            if (sessionStorage.getItem("connection_sate") != null) {
+                obj.connection_sate = sessionStorage.connection_sate;
+            } else {
+                obj.connection_sate             = 0; // we are starting a new connection
+                sessionStorage.connection_sate  = 1; // indicating to the server to continue from where you left off, in the case of reconnection
+            }
+
+            // sending the handshake to server
+            appendText ("info", "Sending "+JSON.stringify(obj)+" to "+server_address)
+            ws.send(JSON.stringify(obj));
+       };
+
+       ws.onmessage = function (evt) {
+          var received_msg = evt.data;
+          alert("Message is received...");
+       };
+
+       ws.onclose = function() {
+          // websocket is closed.
+          alert("Connection is closed...");
+       };
+    } else {
+       // The browser doesn't support WebSocket
+       alert("WebSocket NOT supported by your Browser!");
+    }
+}
+
+/*
     Function to change the colour of info box
     state:
      0: default colour
